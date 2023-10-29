b2c1c6006eee12a9f6af18e943508c3de7f784bd
DrakeAHoffman <drakehoffman99@gmail.com>
2023-10-29 11:07:14 -0700

    added macbook pro product name,price,description

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -4,9 +4,9 @@ const ProductList = () => {
   const products = [
     {
       id: 1,
-      name: 'Apple MacBook Pro',
+      name: 'Apple MacBook Pro 16 inch',
       price: 4000.00,
-      description: ''
+      description: 'Design: The 16-inch MacBook Pro is larger and more powerful than the 13-inch model. It has a premium and sturdy build with a larger display. It is also available in Silver and Space Gray colors.Display: The 16-inch model features a Retina display with a resolution of 3072 x 1920 pixels. It offers excellent color accuracy and is ideal for creative professionals.Performance: The 16-inch MacBook Pro is equipped with Intel processors, and as of my last knowledge update, it had not transitioned to the Apple M1 chip. It offers high performance for demanding tasks.Keyboard: It features a scissor-switch keyboard, which is more reliable than the previous Butterfly keyboard.Battery Life: The 16-inch MacBook Pro provides a reasonable battery life given its larger and more power-hungry components.Audio: It boasts a six-speaker sound system with force-canceling woofers, delivering impressive audio quality.Ports: The 16-inch MacBook Pro includes a variety of ports, including multiple Thunderbolt 3 (USB-C) ports, an HDMI port, and an SD Card slot.Operating System: Like the 13-inch model, it runs macOS and supports a wide range of professional applications.'
     },
     {
       id: 2,
