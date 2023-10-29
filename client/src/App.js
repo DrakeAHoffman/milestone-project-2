fbf4df9394c2516f69b48dba97a4fdc0796fba58
DrakeAHoffman <drakehoffman99@gmail.com>
2023-10-29 11:02:51 -0700

    imported products and deleted uneeded code in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,30 @@
 import axios from "axios";
-<<<<<<< HEAD
-import ProductList from "./components/ProductList";
 
-=======
-import Ratings from './components/Ratings'
+import ProductList from './components/ProductList';
+
+
+//import Ratings from './components/Ratings'
 import Rating from './components/Rating'
->>>>>>> adb309f199412e01f5f535e79a220e7b963d4045
+
 
 import logo from './logo.svg';
 import './App.css';
-import { Component } from "react";
+//import { Component } from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>BOOM</h2>
-        <h1>Andrew has Arrived!!!!! Rawr!!!!! Evan's Here Yo!</h1>
+       
+        <ProductList/>
         <Rating stars="1"/>
         <Rating stars="2"/>
         <Rating stars="3"/>
         <Rating stars="4"/>
         <Rating stars="5"/>
         <Rating stars="0"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Drake</p>
-        <p>HAHHAHAHAHA</p>
-        <p>Was doing everything on a forked clone...</p>
+     
       </header>
     </div>
   );
