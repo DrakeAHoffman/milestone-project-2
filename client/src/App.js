45ada3193fa7bd5a171bd5a778c5d902ab7edfeb
Josh Wedekind <josh@halfnibble.com>
2023-11-02 21:29:38 -0700

    Fetch shopping cart

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,25 @@
-
-
-
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import 'Routes' instead of 'Switch'
-import Home from './components/home';
-import MacBookPage from './components/MacBookPage';
- import ShoppingCart from './components/ShoppingCart'
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import 'Routes' instead of 'Switch'
+import Home from "./components/home";
+import MacBookPage from "./components/MacBookPage";
+import ShoppingCart from "./components/ShoppingCart";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes> {/* Use 'Routes' instead of 'Switch' */}
-          <Route path="/" element={<Home />} />
-          <Route path="/macbooks" element={<MacBookPage/>} />
-          <Route path="/cart" element={<ShoppingCart/>} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {" "}
+                    {/* Use 'Routes' instead of 'Switch' */}
+                    <Route path="/" element={<Home />} />
+                    <Route path="/macbooks" element={<MacBookPage />} />
+                    <Route path="/cart" element={<ShoppingCart />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
-
-
-
 
 export default App;
--- client/src/components/ShoppingCart.jsx
+++ client/src/components/ShoppingCart.jsx
@@ -1,7 +1,34 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 const ShoppingCart = () => {
+    const [cartItems, setCartItems] = useState([]);
+    const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3003";
+
+    useEffect(() => {
+        // Get data from server
+        axios
+            .get(`${API_URL}/cart`)
+            .then((response) => {
+                setCartItems(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
     return (
-        <h1>Shopping Cart</h1>
-    )
-}
+        <div>
+            <h2>Shopping Cart</h2>
+            <ul>
+                {cartItems.map((item) => (
+                    <li key={item.id}>
+                        {item.name} - ${item.price} {item.quantity}x
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default ShoppingCart;
