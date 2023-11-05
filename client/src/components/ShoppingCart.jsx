d2c36f1da2c342b68e05cd77c5f5af8836b66462
DrakeAHoffman <drakehoffman99@gmail.com>
2023-11-05 10:08:38 -0800

    fresh commit

--- client/src/components/ShoppingCart.jsx
+++ client/src/components/ShoppingCart.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import './ShoppingCart.css'
 
 const ShoppingCart = () => {
     const [cartItems, setCartItems] = useState([]);
